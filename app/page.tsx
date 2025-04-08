38dabf7f919f7963bcc5a2617eeeb3fb2ade2a3b
Joe Averbukh <joeaverbukh@gmail.com>
2025-04-08 11:46:42 -0700

    Add shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-4">
       <Button variant="default">Default</Button>
       <Button variant="secondary" size="lg">Default</Button>
       <Button variant="destructive" size="lg">Default</Button>
