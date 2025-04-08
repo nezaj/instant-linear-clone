b5a79e0fd983eaef4a590a6402fadf2fe4838da5
Joe Averbukh <joeaverbukh@gmail.com>
2025-04-08 11:51:58 -0700

    lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex gap-4">
-      <Button variant="default">Default</Button>
+      <Button variant="primary">Default</Button>
       <Button variant="secondary" size="lg">Default</Button>
       <Button variant="destructive" size="lg">Default</Button>
       <Button variant="outline" size="lg">Default</Button>
