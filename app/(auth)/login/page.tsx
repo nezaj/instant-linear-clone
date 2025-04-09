b2d57be8d96e61f36172676d0a7ae78e2d4c0c98
Joe Averbukh <joeaverbukh@gmail.com>
2025-04-09 00:10:02 -0700

    Add otp

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,30 +3,56 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from 'sonner';
+import { Loader } from "lucide-react";
+
 import {
   Card,
   CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import {
   InputOTP,
   InputOTPGroup,
   InputOTPSlot,
 } from "@/components/ui/input-otp"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { toast } from 'sonner';
 
 import { db } from "@/lib/db";
+
+const EmailSchema = z.object({
+  email: z.string().email(),
+});
+
+const CodeSchema = z.object({
+  code: z.string().length(6, "Code must be 6 characters long"),
+});
+
 
 export default function LoginPage() {
   const router = useRouter();
   const { isLoading, user } = db.useAuth();
 
   useEffect(() => {
-    if (!isLoading && user) {
+    if (user) {
       router.push("/");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user, isLoading]);
+  }, [user]);
 
   if (isLoading || user) {
     return null;
@@ -38,111 +64,170 @@ export default function LoginPage() {
 }
 
 const LoginCard = () => {
-  const [sentEmail, setSentEmail] = useState("");
+  const [email, setEmail] = useState("");
+  const [isEmailStep, setIsEmailStep] = useState(true);
+
+  const handleSendEmail = (email: string) => {
+    setEmail(email);
+    setIsEmailStep(false);
+  }
+
+  const handleBack = () => {
+    setIsEmailStep(true);
+  }
 
   return (
     <Card className="w-full h-full md:w-[480px] border-none shadow-none">
       <CardContent className="p-8">
-        {!sentEmail ? (
-          <EmailStep onSendEmail={setSentEmail} />
+        {isEmailStep ? (
+          <EmailStep onSendEmail={handleSendEmail} initialEmail={email} />
         ) : (
-          <CodeStep sentEmail={sentEmail} />
+          <CodeStep
+            email={email}
+            onBack={handleBack}
+          />
         )}
       </CardContent>
     </Card>
   );
 }
 
-function EmailStep({ onSendEmail }: { onSendEmail: (email: string) => void }) {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const inputEl = inputRef.current!;
-    const email = inputEl.value;
-    onSendEmail(email);
-    db.auth.sendMagicCode({ email }).catch((err) => {
+interface EmailStepProps {
+  onSendEmail: (email: string) => void;
+  initialEmail: string;
+}
+
+function EmailStep({ onSendEmail, initialEmail }: EmailStepProps) {
+  const form = useForm<z.infer<typeof EmailSchema>>({
+    resolver: zodResolver(EmailSchema),
+    defaultValues: {
+      email: initialEmail,
+    },
+  })
+
+  const onSubmit = (data: z.infer<typeof EmailSchema>) => {
+    onSendEmail(data.email);
+    db.auth.sendMagicCode({ email: data.email }).catch((err) => {
       alert("Uh oh :" + err.body?.message);
       onSendEmail("");
     });
-  };
+  }
+
   return (
-    <form
-      key="email"
-      onSubmit={handleSubmit}
-      className="flex flex-col space-y-4"
-    >
-      <h2 className="text-xl font-bold">{"Let's log you in"}</h2>
-      <p className="text-gray-700">
-        Enter your email to receive a verification code.
-      </p>
-      <input
-        ref={inputRef}
-        type="email"
-        className="border border-gray-300 px-3 py-1  w-full"
-        placeholder="Enter your email"
-        required
-        autoFocus
-      />
-      <Button type="submit">
-        Send Code
-      </Button>
-    </form >
+    <Form {...form}>
+      <form
+        key="email"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-4"
+      >
+        <CardHeader>
+          <CardTitle>{"Let's log you in"}</CardTitle>
+          <CardDescription>
+            Enter your email to receive a verification code.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Enter your email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </CardContent >
+        <CardFooter>
+          <Button type="submit" className="w-full">
+            Send Code
+          </Button>
+        </CardFooter>
+      </form >
+    </Form>
   );
 }
 
-function CodeStep({ sentEmail }: { sentEmail: string }) {
-  const [code, setCode] = React.useState<string>("");
+interface CodeStepProps {
+  email: string;
+  onBack: () => void;
+}
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log("SUBMIT!", code)
-    e.preventDefault();
+function CodeStep({ email, onBack }: CodeStepProps) {
+  const [isVerifying, setIsVerifying] = useState(false);
+  const form = useForm<z.infer<typeof CodeSchema>>({
+    resolver: zodResolver(CodeSchema),
+    defaultValues: {
+      code: "",
+    },
+  })
 
-    if (code.length < 6) {
-      toast.error("Please enter the complete verification code.");
-      return;
-    }
-
-    db.auth.signInWithMagicCode({ email: sentEmail, code }).catch(() => {
-      toast.error("This code is invalid. Please try again.");
-    });
-  };
+  const onSubmit = (data: z.infer<typeof CodeSchema>) => {
+    setIsVerifying(true);
+    db.auth.signInWithMagicCode({ email: email, code: data.code })
+      .then(() => {
+        toast.success("Logged in successfully!");
+      })
+      .catch(() => {
+        setIsVerifying(false);
+        toast.error("This code is invalid. Please try again.");
+      });
+  }
 
   return (
-    <form
-      key="code"
-      onSubmit={handleSubmit}
-      className="flex flex-col space-y-4"
-    >
-      <h2 className="text-xl font-bold">Enter your code</h2>
-      <p className="text-gray-700">
-        We sent an email to <strong>{sentEmail}</strong>. Check your email, and
-        paste the code you see.
-      </p>
-
-      <div className="mx-auto">
-        <InputOTP
-          value={code}
-          onChange={setCode}
-          maxLength={6}
-        >
-          <InputOTPGroup>
-            <InputOTPSlot index={0} className="size-12 text-xl" />
-            <InputOTPSlot index={1} className="size-12 text-xl" />
-            <InputOTPSlot index={2} className="size-12 text-xl" />
-            <InputOTPSlot index={3} className="size-12 text-xl" />
-            <InputOTPSlot index={4} className="size-12 text-xl" />
-            <InputOTPSlot index={5} className="size-12 text-xl" />
-          </InputOTPGroup>
-        </InputOTP>
-      </div>
-
-      <Button
-        type="submit"
-        className="px-3 py-1 bg-blue-600 text-white font-bold hover:bg-blue-700 w-full"
-        onClick={() => handleSubmit}
+    <Form {...form}>
+      <form
+        key="code"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-4"
       >
-        Verify Code
-      </Button>
-    </form>
+        <CardHeader>
+          <CardTitle>Enter your code</CardTitle>
+          <CardDescription>
+            We sent an email to <strong>{email}</strong>. Check your email,
+            and paste the code you see.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-center">
+          <FormField
+            control={form.control}
+            name="code"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputOTP
+                    value={field.value}
+                    onChange={field.onChange}
+                    maxLength={6}
+                  >
+                    <InputOTPGroup className="gap-2">
+                      <InputOTPSlot index={0} className="size-10 text-xl" />
+                      <InputOTPSlot index={1} className="size-10 text-xl" />
+                      <InputOTPSlot index={2} className="size-10 text-xl" />
+                      <InputOTPSlot index={3} className="size-10 text-xl" />
+                      <InputOTPSlot index={4} className="size-10 text-xl" />
+                      <InputOTPSlot index={5} className="size-10 text-xl" />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </CardContent>
+        <CardFooter className="flex flex-col space-y-4">
+          <Button type="submit" className="w-full" disabled={isVerifying}>
+            {isVerifying && <Loader className="animate-spin -ml-2" />}
+            {isVerifying ? "Verifying..." : "Verify Code"}
+          </Button>
+          <CardDescription className="inline">
+            Didn&apos;t receive a code? Wait a moment or{" "}
+            <Button type="button" variant="link" onClick={onBack} size="sm" className="p-0" disabled={isVerifying}>try again.</Button>
+          </CardDescription>
+        </CardFooter>
+      </form>
+    </Form>
   );
 }
